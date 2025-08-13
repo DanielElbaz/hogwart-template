7eee14a767ae040573e47b8752005d899e841c6c
Daniel <daniel.elbaz2000@gmail.com>
2025-08-13 15:09:00 +0300

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import { CATEGORIES, CHARMS_DATA, POTIONS_DATA } from './Constants';
 import HogwartNavbar from './components/Header/HogwartNavbar';
@@ -13,8 +13,8 @@ const App = (props) => {
   const [potions, setPotions] = useState(POTIONS_DATA);
   const [charms, setCharms] = useState(CHARMS_DATA);
   const STATE_MAP = {
-    potions:potions,
-    charms:charms
+    potions: potions,
+    charms: charms
   }
 
   const getCategoryData = (category) => {
@@ -22,12 +22,18 @@ const App = (props) => {
   }
 
   return (
-    <div>
+    <>
+      <BrowserRouter>
         <div className="App">
           <HogwartNavbar />
         </div>
-        {/* add routes and route here */}
-    </div>
+        <Routes>
+          <Route path="/" element={<Home categories={categories} />} />
+          <Route path="/:category" element={<Entities getCategoryData={getCategoryData} />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
 
   );
 }
